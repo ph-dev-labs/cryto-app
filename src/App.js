e1d75489017f724d685d02ba244ce9332f67c815
ph-dev-labs <nwosuchimezie95@gmail.com>
2022-06-17 12:27:32 +0200

    finished crypto-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+import {Layout, Typography, Space} from "antd"
+import "./App.css"
+
+import {Navbar, CryptoDetails, Cryptocurrencies , Homepage, Exchanges, News} from './components'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+           <div className="routes">
+             <Routes>
+               <Route exact path="/" element ={<Homepage/>} /> 
+               <Route path="/exchanges" element={<Exchanges />} />
+               <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+               <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+               <Route path="/news" element={<News />} />
+             </Routes>
+             </div>
+        </Layout>
+         <div className="footer">
+          <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+            Sultan-crypto <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+        </div>
+   </div>
   );
 }
 
